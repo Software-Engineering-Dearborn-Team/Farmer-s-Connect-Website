f4d5131f9b6318b5c78c013ddbdeb9241de23cbd
joeB0dy <sjose@umich.edu>
2023-03-28 22:03:24 -0400

    Testing

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+ 
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
